8b0eb970149da578eccc3b31364ef0271d74c77d
Sheng Yong <shengyong@oppo.com>
2023-06-27 20:21:53 +0800

    f2fs: only set release for file that has compressed data
    If a file is not comprssed yet or does not have compressed data,
    for example, its data has a very low compression ratio, do not
    set FI_COMPRESS_RELEASED flag.
    
    Signed-off-by: Sheng Yong <shengyong@oppo.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -3577,12 +3577,14 @@ static int f2fs_release_compress_blocks(struct file *filp, unsigned long arg)
 	if (ret)
 		goto out;
 
+	if (!atomic_read(&F2FS_I(inode)->i_compr_blocks)) {
+		ret = -EPERM;
+		goto out;
+	}
+
 	set_inode_flag(inode, FI_COMPRESS_RELEASED);
 	inode->i_ctime = current_time(inode);
 	f2fs_mark_inode_dirty_sync(inode, true);
-
-	if (!atomic_read(&F2FS_I(inode)->i_compr_blocks))
-		goto out;
 
 	down_write(&F2FS_I(inode)->i_gc_rwsem[WRITE]);
 	down_write(&F2FS_I(inode)->i_mmap_sem);
