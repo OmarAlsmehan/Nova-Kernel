c40094eaf2b3fc54f6a5b779125382ee46b88b64
Salvo Giangreco <giangrecosalvo9@gmail.com>
2026-02-18 10:45:42 +0100

    Revert "mm/page_alloc.c: ratelimit allocation failure warnings more aggressively"
    This reverts commit 1be334e5c0886197cc82923ff0ac5836111b7b57.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3890,6 +3890,10 @@ try_this_zone:
 static void warn_alloc_show_mem(gfp_t gfp_mask, nodemask_t *nodemask)
 {
 	unsigned int filter = SHOW_MEM_FILTER_NODES;
+	static DEFINE_RATELIMIT_STATE(show_mem_rs, HZ, 1);
+
+	if (!__ratelimit(&show_mem_rs))
+		return;
 
 	/*
 	 * This documents exceptions given to allocations in certain
@@ -3911,7 +3915,8 @@ void warn_alloc(gfp_t gfp_mask, nodemask_t *nodemask, const char *fmt, ...)
 {
 	struct va_format vaf;
 	va_list args;
-	static DEFINE_RATELIMIT_STATE(nopage_rs, 10*HZ, 1);
+	static DEFINE_RATELIMIT_STATE(nopage_rs, DEFAULT_RATELIMIT_INTERVAL,
+				      DEFAULT_RATELIMIT_BURST);
 
 	if ((gfp_mask & __GFP_NOWARN) ||
 	     !__ratelimit(&nopage_rs) ||
