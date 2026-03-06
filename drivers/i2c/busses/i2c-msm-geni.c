nekoshirro/platform_kernel_xiaomi_marble

i2c: i2c-msm-geni: Do i2c transfer if IOS in good state
This change ensures the i2c IO lines are in proper state before starting
the new transfer. This way we dont run into post transfer issues like
NACK/cancel and related unwanted operation if lines are not Pulled high.

Change-Id: I88b22ac49e23f1946f897eaeb2f2c1f588ef05a0
Signed-off-by: Mukesh Kumar Savaliya <quic_msavaliy@quicinc.com>

drivers/i2c/busses/i2c-msm-geni.c
@@ -1001,6 +1001,7 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 {
 	struct geni_i2c_dev *gi2c = i2c_get_adapdata(adap);
 	int i, ret = 0, timeout = 0;
+	u32 geni_ios = 0;
 
 	gi2c->err = 0;
 
@@ -1021,6 +1022,15 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 			pm_runtime_set_suspended(gi2c->dev);
 			return ret;
 		}
+	}
+
+	geni_ios = geni_read_reg_nolog(gi2c->base, SE_GENI_IOS);
+	if ((geni_ios & 0x3) != 0x3) { //SCL:b'1, SDA:b'0
+		I2C_LOG_ERR(gi2c->ipcl, true, gi2c->dev,
+			"IO lines in bad state, Power the slave\n");
+		pm_runtime_mark_last_busy(gi2c->dev);
+		pm_runtime_put_autosuspend(gi2c->dev);
+		return -ENXIO;
 	}
 
 	I2C_LOG_DBG(gi2c->ipcl, false, gi2c->dev,

OmarAlsmehan/Nova-Kernel

drivers/i2c/busses/i2c-msm-geni.c
@@ -1061,6 +1061,7 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 {
 	struct geni_i2c_dev *gi2c = i2c_get_adapdata(adap);
 	int i, ret = 0, timeout = 0;
+	u32 geni_ios = 0;
 
 	gi2c->err = 0;
 	atomic_set(&gi2c->is_xfer_in_progress, 1);
@@ -1084,6 +1085,15 @@ static int geni_i2c_xfer(struct i2c_adapter *adap,
 			atomic_set(&gi2c->is_xfer_in_progress, 0);
 			return ret;
 		}
+	}
+
+	geni_ios = geni_read_reg_nolog(gi2c->base, SE_GENI_IOS);
+	if ((geni_ios & 0x3) != 0x3) { //SCL:b'1, SDA:b'0
+		GENI_SE_ERR(gi2c->ipcl, true, gi2c->dev,
+			"IO lines in bad state, Power the slave\n");
+		pm_runtime_mark_last_busy(gi2c->dev);
+		pm_runtime_put_autosuspend(gi2c->dev);
+		return -ENXIO;
 	}
 
 	// WAR : Complete previous pending cancel cmd